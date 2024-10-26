eecbb456cfe35571c423edf72f712e384bd87b12
Melvin jerry <+>
2024-10-26 12:40:28 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        matches = []
+        sorted_word = sorted(self.word)  # Sort the letters of the original word
+
+        for candidate in possible_anagrams:
+            if sorted_word == sorted(candidate):  # Compare sorted letters
+                matches.append(candidate)  # If they match, add to results
+        
+        return matches
